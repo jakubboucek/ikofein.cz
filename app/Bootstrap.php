d06070f4d5c07c786e7aae56484fce1f69a180be
Jakub Bouček <pan@jakubboucek.cz>
2022-03-06 01:38:56 +0100

    Remove Composer consistency package

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace App;
 
-use JakubBoucek\ComposerConsistency\ComposerConsistency;
 use Nette\Configurator;
-use Nette\InvalidArgumentException;
-use Nette\NotSupportedException;
 use Redbitcz\DebugMode\Detector;
-use RuntimeException;
-use Tracy\Debugger;
-
 
 class Bootstrap
 {
@@ -21,8 +15,6 @@ class Bootstrap
 
         $configurator->setDebugMode(Detector::detect());
         $configurator->enableDebugger(__DIR__ . '/../log', 'pan@jakubboucek.cz');
-
-        self::checkVendorConsistency();
 
         $configurator->setTimeZone('Europe/Prague');
         $configurator->setTempDirectory(__DIR__ . '/../temp');
@@ -36,13 +28,5 @@ class Bootstrap
         $configurator->addConfig(__DIR__ . '/../local/Config/config.neon');
 
         return $configurator;
-    }
-
-
-    protected static function checkVendorConsistency(): void
-    {
-        if (Debugger::$productionMode === false && class_exists(ComposerConsistency::class)) {
-            ComposerConsistency::rootDir(__DIR__ . '/..')->validate();
-        }
     }
 }
