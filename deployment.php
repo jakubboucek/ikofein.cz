cd0fcd6011ce8a97b5b10d16df9ef82f5b46db04
Jakub Bouček <pan@jakubboucek.cz>
2024-11-10 22:18:32 +0100

    Deploy: Add FTP Deployment

---
+++ deployment.php
@@ -0,0 +1,29 @@
+<?php
+
+$credentials = require __DIR__ . '/.deployment-credentials.php';
+
+return [
+    'Production' => [
+        'remote' => 'ftps://prince.bukajuv.net/www',
+        'local' => __DIR__,
+        'user' => $credentials['user'],
+        'password' => $credentials['password'],
+        'test' => false,
+        'include' => [
+            '/app',
+            '/libs',
+            '/www',
+            '/vendor',
+        ],
+        'ignore' => [
+            '/www/.well-known',
+            '/temp/*',
+            '/log',
+        ],
+        'allowDelete' => true,
+        //'after' => ['https://www.ikofein.cz/deployment.php?after'],
+    ],
+    'tempDir' => __DIR__ . '/temp',
+    'colors' => true,
+    'log' => __DIR__ . '/log/deployment.log'
+];
